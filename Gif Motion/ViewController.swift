38b474065212e76b85bce8ccbbe1c24cba84c3aa
Martin Chibwe <mchibwe01@gmail.com>
2017-10-27 12:27:32 -0700

    starting project

---
+++ Gif Motion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gif Motion
+//
+//  Created by Martin Chibwe on 10/27/17.
+//  Copyright © 2017 Martin Chibwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
